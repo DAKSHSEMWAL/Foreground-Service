414aeac18db1cc69f788ba0849e177fdff2e2f4c
Daksh Semwal <kuro.aquasoft@gmail.com>
2019-03-27 12:04:27 +0530

    Initial commit

---
+++ app/src/main/java/com/kuro/daksh/foregroundnotification/ExampleService.java
@@ -0,0 +1,64 @@
+package com.kuro.daksh.foregroundnotification;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import static com.kuro.daksh.foregroundnotification.App.CHANNEL_ID;
+import static com.kuro.daksh.foregroundnotification.App.sendTechLoc;
+
+public class ExampleService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Example Service")
+                .setContentText(input)
+                .setColor(Color.RED)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        getNotificationIcon()) )
+                .setSmallIcon(getNotificationIcon())
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+
+        //do heavy work on a background thread
+        //stopSelf();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private int getNotificationIcon() {
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? getApplicationInfo().icon: R.drawable.ic_my_location;
+    }
+}
